528c09de6970c07ad23c58664e9f8b69a0244d00
YueXingTong <d00234628@student.dkit.ie>
2022-11-15 23:01:28 +0000

    Store user data in Firestore

--- app/src/main/java/com/example/ezhoop/activities/RegisterActivity.java
+++ app/src/main/java/com/example/ezhoop/activities/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.ezhoop.activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,22 +15,26 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ezhoop.App;
 import com.example.ezhoop.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private Context context;
+    private final SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
-    private EditText inputFullName, inputEmail, inputPassword;
+    private EditText inputFullName, inputEmail, inputPassword, inputCountry;
     private DatePicker inputDateOfBirth;
     private RadioGroup inputGender;
     private Button btnRegister;
@@ -47,11 +54,15 @@ public class RegisterActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         inputEmail = findViewById(R.id.input_email);
-        inputPassword = findViewById(R.id.input_password1);
+        inputPassword = findViewById(R.id.input_password);
 
         inputFullName = findViewById(R.id.input_full_name);
-        inputDateOfBirth = findViewById(R.id.input_date_of_birth);
         inputGender = findViewById(R.id.input_gender);
+
+        inputDateOfBirth = findViewById(R.id.input_date_of_birth);
+        inputDateOfBirth.setMaxDate(new Date().getTime());
+
+        inputCountry = findViewById(R.id.input_country);
 
         btnRegister = findViewById(R.id.btn_register);
         btnRegister.setOnClickListener(v -> register());
@@ -72,13 +83,52 @@ public class RegisterActivity extends AppCompatActivity {
     private void register() {
         String email = inputEmail.getText().toString();
         String password = inputPassword.getText().toString();
+        String fullName = inputFullName.getText().toString();
+        String country = inputCountry.getText().toString();
+
+        if (email.equals("") || password.equals("") || fullName.equals("")) {
+            Toast.makeText(context, "Fields cannot be empty.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         FirebaseAuth firebaseAuth = ((App) getApplicationContext()).firebaseAuth;
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
             if (task.isSuccessful()) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
 
-                // TODO: Add user data to Firestore w/ UID
+                String uid = user.getUid();
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(inputDateOfBirth.getYear(), inputDateOfBirth.getMonth(), inputDateOfBirth.getDayOfMonth());
+                String dateOfBirth = sdf.format(calendar.getTime());
+
+                RadioButton radioButton = inputGender.findViewById(inputGender.getCheckedRadioButtonId());
+                String gender = radioButton.getText().toString();
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                // Create a new user with a first and last name
+                Map<String, Object> data = new HashMap<>();
+                data.put("fullName", fullName);
+                data.put("dateOfBirth", dateOfBirth);
+                data.put("gender", gender);
+                data.put("country", country);
+
+                db.collection("users").document(uid).set(data)
+                        .addOnSuccessListener(documentReference -> {
+                            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                            prefs.edit()
+                                    .putString("fullName", fullName)
+                                    .putString("dateOfBirth", dateOfBirth)
+                                    .putString("gender", gender)
+                                    .putString("country", country)
+                                    .apply();
+
+                            Intent intent = new Intent(context, MainActivity.class);
+                            startActivity(intent);
+                            finishAffinity();
+                        })
+                        .addOnFailureListener(e -> Log.e("Error", "Error adding user document", e));
             } else {
                 Toast.makeText(context, "Registration failed.", Toast.LENGTH_SHORT).show();
             }
