7669dc6bb15b0dd354df828c7768ac672b33f0ae
jeremysoh0210 <jeremysoh222@gmail.com>
2022-12-18 18:02:23 +0000

    unit testing created by Clinton

--- app/src/androidTest/java/com/example/ezhoop/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ezhoop/ExampleInstrumentedTest.java
@@ -1,6 +1,8 @@
 package com.example.ezhoop;
 
 import android.content.Context;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -10,6 +12,10 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+
+
+import com.example.ezhoop.activities.LoginActivity;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -17,10 +23,26 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private EditText usernameField, passwordField;
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.ezhoop", appContext.getPackageName());
     }
+
+//    public void testUserNameAndPasswordFields()
+//    {
+//        usernameField = findViewByID(R.id.input_email);
+//        passwordField = findViewByID(R.id.input_password);
+//
+//        assertNotNull(usernameField);
+//        assertNotNull(passwordField);
+//
+//        usernameField.requestFocus();
+//        passwordField.requestFocus();
+//
+//
+//    }
 }
--- app/src/test/java/com/example/ezhoop/ExampleUnitTest.java
+++ app/src/test/java/com/example/ezhoop/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,77 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    //
+    @RunWith(RobolectricTestRunner.class)
+    public class LoginActivityTest {
+
+        private LoginActivity activity;
+        private EditText inputEmail;
+        private EditText inputPassword;
+        private Button btnLogin;
+
+        @Before
+        public void setUp() {
+            activity = Robolectric.buildActivity(LoginActivity.class).create().get();
+            inputEmail = activity.findViewById(R.id.input_email);
+            inputPassword = activity.findViewById(R.id.input_password);
+            btnLogin = activity.findViewById(R.id.btn_login);
+        }
+
+        @Test
+        public void testLoginButton() {
+            // Test empty email and password
+            inputEmail.setText("");
+            inputPassword.setText("");
+            btnLogin.performClick();
+            assertEquals("Email & password cannot be empty.", ShadowToast.getTextOfLatestToast());
+
+            // Test invalid email
+            inputEmail.setText("invalid");
+            inputPassword.setText("password");
+            btnLogin.performClick();
+            // Assert that login failed toast is shown
+            assertEquals("Login failed.", ShadowToast.getTextOfLatestToast());
+
+            // Test valid email and password
+            inputEmail.setText("valid@email.com");
+            inputPassword.setText("password");
+            btnLogin.performClick();
+            // Assert that the main activity is opened
+            assertNotNull(activity.getNextStartedActivity());
+            assertEquals(MainActivity.class.getCanonicalName(), activity.getNextStartedActivity().getComponent().getClassName());
+        }
+
+        @Test
+        public void testTogglePasswordButton_togglesPasswordField() {
+            // Set the password field to a known value
+            inputPassword.setText("password123");
+
+            // Click the toggle password button
+            btnTogglePassword.performClick();
+
+            // Check the value of the password field after it has been transformed
+            assertEquals("password123", inputPassword.getText().toString());
+
+            // Click the toggle password button again
+            btnTogglePassword.performClick();
+
+            // Check the value of the password field after it has been transformed again
+            assertEquals("password123", inputPassword.getText().toString());
+        }
+
+        @Test
+        public void testLoginButton_triggersLoginMethod() {
+            // Set up a mock click listener to verify that it was called
+            View.OnClickListener mockListener = Mockito.mock(View.OnClickListener.class);
+            btnLogin.setOnClickListener(mockListener);
+
+            // Click the login button
+            btnLogin.performClick();
+
+            // Verify that the mock listener was called
+            Mockito.verify(mockListener).onClick(btnLogin);
+        }
+        //
 }
